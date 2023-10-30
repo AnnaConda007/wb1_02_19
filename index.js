9c975b54594d2fc3d9c3dea1a59c09f76fe6196d
Anna Hrulckova <annahrulkova@yandex.ru>
2023-10-30 23:39:08 +0400

    url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const accessToken = localStorage.getItem("vkAccessToken");
 if (!accessToken) {
   const baseUrl = "https://oauth.vk.com/authorize";
   const clientId = "51780688";
-  const redirectUri = "http://127.0.0.1:5500/19/redirect.html";
+  const redirectUri = "https://annaconda007.github.io/wb1_02_19//redirect";
   const authUrl = `${baseUrl}?client_id=${clientId}&redirect_uri=${redirectUri}`;
   window.location.href = authUrl;
 } else {
--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const PORT = 3000;
 
 const CLIENT_ID = "51780688";
 const CLIENT_SECRET = "b1bwhzPGfiF2fN1f8FwE";
-const REDIRECT_URI = "http://127.0.0.1:5500/19/redirect.html";
-
+const REDIRECT_URI = "https://annaconda007.github.io/wb1_02_19//redirect";
 
 app.get("/get-token", async (req, res) => {
   const code = req.query.code;
@@ -34,7 +33,7 @@ app.get("/get-token", async (req, res) => {
   }
 });
 
-app.get('/vkdata', async (req, res) => {
+app.get("/vkdata", async (req, res) => {
   const API_URL = "https://api.vk.com/method/wall.get";
   const ACCESS_TOKEN = req.query.access_token;
   const OWNER_ID = "-34215577";
@@ -42,18 +41,16 @@ app.get('/vkdata', async (req, res) => {
   const COUNT = 5;
   const OFFSET = req.query.offset || 0;
   try {
-      const response = await fetch(
-        `${API_URL}?access_token=${ACCESS_TOKEN}&owner_id=${OWNER_ID}&count=${COUNT}&offset=${OFFSET}&v=${VERSION}`
-        );
-      const data = await response.json();
-      res.json(data);
+    const response = await fetch(
+      `${API_URL}?access_token=${ACCESS_TOKEN}&owner_id=${OWNER_ID}&count=${COUNT}&offset=${OFFSET}&v=${VERSION}`
+    );
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
-      console.error("Ошибка:", error);
-      res.status(500).send('Ошибка при получении данных из VK API.');
+    console.error("Ошибка:", error);
+    res.status(500).send("Ошибка при получении данных из VK API.");
   }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
